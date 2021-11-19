54d4939e0fb04f713cf1c20d0f7bee46c87fb695
adamwang <falco1016@gmail.com>
2021-11-19 17:44:56 +0800

    First commit

--- silkrodeTestProject/ViewController.swift
+++ silkrodeTestProject/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getUserData()
+    }
+    
+    func getUserData() {
+        
+        let userUrlString = "https://api.github.com/users/0"
+        guard let url = URL(string: userUrlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            do {
+                
+                print("")
+                print(data)
+//                let user = try JSONDecoder().decode(User.self, from: data)
+                
+            } catch let jsonErr {
+                print("Error serializing:\(jsonErr)")
+            }
+        }.resume()
+        
+        
+        
     }
 
 
